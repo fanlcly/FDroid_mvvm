ed5bb5ae45465a69314987fa4dcfac5c85303d05
fanlcly <fanlcly@126.com>
2022-11-27 12:13:49 +0800

    日志的添加

--- app/src/main/java/com/jjl/fdroid_mvvm/net/HttpClient.kt
+++ app/src/main/java/com/jjl/fdroid_mvvm/net/HttpClient.kt
@@ -4,6 +4,7 @@ import com.jjl.mvvm.net.BaseHttpClient
 import com.jjl.mvvm.net.NetProvider
 
 object HttpClient : BaseHttpClient() {
+    // 如果不需要配置可以不用重写
     override var provider: NetProvider? = MyNetProvider()
 
     val service by lazy { getService(ApiService::class.java, ApiService.BASE_URL) }
